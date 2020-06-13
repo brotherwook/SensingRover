73d733d2349df6a20509b68c54bff3506e56e93b
sjpark-dev <sjpark.dev@gmail.com>
2020-06-13 22:28:49 +0900

    Camera.py 수정

--- gpio/Camera.py
+++ gpio/Camera.py
@@ -4,27 +4,27 @@ import base64
 
 class Camera(threading.Thread):
     def __init__(self):
-        self.videoCapture = cv2.VideoCapture(0)
-        self.videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-        self.videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        try: # in case camera fails to load properly
+            self.videoCapture = cv2.VideoCapture(0)
+            self.videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+            self.videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        except: # release camera if opened wrongly
+            if self.videoCapture.isOpened():
+                self.videoCapture.release()
         self.message = None
         super().__init__(daemon=True)
         super().start()
 
     def run(self):
-        try:
-            while True:
-                if self.videoCapture.isOpened():
-                    retval, frame = self.videoCapture.read()
-                    if not retval:
-                        print("video capture fail")
-                        continue
-                    self.encode(frame)
-                else:
-                    print("video not open")
-        finally:
-            self.videoCapture.release()
-            print("videoCapture released")
+        while True:
+            if self.videoCapture.isOpened():
+                retval, frame = self.videoCapture.read()
+                if not retval:
+                    print("video capture fail")
+                    continue
+                self.encode(frame)
+            else:
+                print("video not open")
 
     def encode(self, frame):
         retval, bytes = cv2.imencode(".jpg", frame)
